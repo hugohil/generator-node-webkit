990d32065093c58da15a0876d4e70ba5c51ea9ee
hugo <hugo@soixantecircuits.fr>
2015-02-13 16:26:48 +0100

    change server base

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -222,7 +222,7 @@ module.exports = function (grunt) {
       server: {
         options: {
           port: 9001,
-          base: 'app',
+          base: 'app/views',
           livereload: true
         }
       }
